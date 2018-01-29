a61b4054521c1fceaf2ad48aa316e99c423b0434
Saif M <sikhlana@gmail.com>
2018-01-29 18:46:34 +0600

    Trial run.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+Vue.component('calculator', require('./components/Calculator.vue'));
+
 const app = new Vue({
     el: '#app',
 });
---
+++ assets/js/components/Calculator.vue
@@ -0,0 +1,28 @@
+<template>
+    <form class="ui form" @submit.prevent="calculate">
+        <div class="field">
+            <label for="ctrl_value">Input:</label>
+            <input type="number" v-model="value" id="ctrl_value">
+        </div>
+    </form>
+</template>
+<script>
+    export default {
+        data()
+        {
+            return {
+                value: 1,
+                type: 'deg',
+                calc: 'sin',
+            };
+        },
+
+        methods:
+        {
+            calculate()
+            {
+
+            }
+        }
+    }
+</script>
